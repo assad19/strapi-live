f138d7bda0e116449c59128c8d4b023168e0131a
assad19 <154305824+assad19@users.noreply.github.com>
2024-05-22 14:22:23 +0500

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','tobemodified'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
+
   transfer: {
     token: {
       salt: env('TRANSFER_TOKEN_SALT'),
